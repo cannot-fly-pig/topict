3a33947f3861e3da48bb7f53ec1ace2660151b11
cannot-fly-pig <document.sabakan@gmail.com>
2020-01-24 00:28:17 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/index'
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: HelloWorld
+    }
+  ]
+})
